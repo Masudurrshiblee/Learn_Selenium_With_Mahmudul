1e8ed7740cb988b2f05c434f4b5e70ec3224d429
Masudurrshiblee <mrshiblee24@gmail.com>
2021-10-16 12:06:18 -0400

    first day automation code commiting

--- src/amazon/HomePage.java
+++ src/amazon/HomePage.java
@@ -1,4 +1,28 @@
 package amazon;
 
+import org.openqa.selenium.WebDriver;
+import  org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class HomePage {
+    public static void main(String[] args) throws InterruptedException {
+
+// This code is Firefox browser
+//       String fireFoxDriverPath="BrowserDriver/Windows/geckodriver-v0.30.0-win64/geckodriver.exe ";
+//        System.setProperty("webdriver.gecko.driver",fireFoxDriverPath);
+//       WebDriver driver= new FirefoxDriver();
+//       driver.get("https://www.amazon.com/");
+
+        // This code is for Chrome Browser
+        String chromeDriverPath="BrowserDriver/Windows/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver",chromeDriverPath);
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        driver.manage().window().fullscreen();
+        driver.manage().deleteAllCookies();
+        Thread.sleep(5000);
+        driver.close();
+
+    }
+
 }
